b571577c425035dec785e9b68002185b0df1fd75
Mandy Hubbard <mandy.hubbard@chainguard.dev>
2025-09-04 12:55:22 -0500

    Change to md5

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        hash = hashlib.sha256(post_data).hexdigest()
+        hash = hashlib.md5(post_data).hexdigest()
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
